14c67436843e8f76bf9d2d009d9608997caf20a8
alwanrizky <alwanrizky7@gmail.com>
2021-06-18 10:34:04 +0700

    mengubah route agar saat pertama dibuka menampilkan halaman login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login'); // saat membuka diarahkan ke login
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
