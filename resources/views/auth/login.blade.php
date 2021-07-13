06b251ec2a3fa7cbbd173eff0543205f70c36688
alwanrizky <alwanrizky7@gmail.com>
2021-07-13 16:17:36 +0700

    mengubah UI sesuai dengan mockup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,57 @@
-@extends('layouts.header')
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('title','Nomor Surat Teknik Informatika')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-@section('container')
+    <link rel="stylesheet" href="{{URL::asset('css/login.css')}}"> 
+</head>
+<body>
+    <form method="POST" action="{{ route('login') }}">
+    @csrf
+        <div class="container mx-auto">
+            <x-jet-validation-errors class="mb-4" />
 
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <!-- hide logo bawaan -->
-            <!-- <x-jet-authentication-card-logo />  -->
-        </x-slot>
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <table align="center">
+                <tr>
+                    <td><img src="{{asset('asset/Logo_UNPAR.png')}}" width="100" height="100" class="rounded mx-auto d-block image" alt=""></td>
+                </tr>
+                <tr>
+                    <td>
+                    <h1>
+                        NOSURAT <br>
+                        FTIS <br>
+                        UNPAR
+                    </h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="Email" required autofocus />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Password"/>
+                    </td>
+                </tr>
 
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
-@endsection
+                <tr>
+                    <td>
+                        <x-jet-button class="btn btn-primary btn-sm btn-block">
+                            {{ __('Log in') }}
+                        </x-jet-button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+    
+</body>
+</html>
