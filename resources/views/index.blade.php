8c6fed38018167ee0b30a77320f1d88d70f62273
alwanrizky <alwanrizky7@gmail.com>
2021-07-13 14:51:39 +0700

    menambahkan UI untuk index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{URL::asset('css/index.css')}}"> 
+</head>
+<body >
+    <div>
+        <table align="center">
+            <td><img src="{{asset('asset/Logo_UNPAR.png')}}" width="100" height="100" class="rounded mx-auto d-block image" alt=""></td>
+            <td>
+                <h1>Fakultas Teknologi <br>
+                Informasi & Sains </h1>
+            </td>
+            <td class="vl">
+            </td>
+            <td>
+                <h2>
+                    Nomor Surat <br>
+                    <br>
+                    <button class="btn btn-primary" onclick="window.location='/login'">
+                        Login
+                    </button>
+                </h2>
+            </td>
+        </table>
+    </div>
+    
+</body>
+</html>
