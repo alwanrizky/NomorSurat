d67b5a6a6c4a6cd89beaea72e55726563b80a069
alwanrizky <alwanrizky7@gmail.com>
2021-08-05 10:45:14 +0700

    mengubah page generate result dan menambahkan fungsi untuk copy nomor surat

--- resources/views/generate-result.blade.php
+++ resources/views/generate-result.blade.php
@@ -1,12 +1,21 @@
 <x-app-layout>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<div class="container"  align="center" style="width:800px; margin:0 auto; background: #00c8f8; padding-top:2%; padding-bottom: 2%; border-radius: 10px; margin-top: 10%">
-    <div style="background: #91e4fb; width: 80%; padding-top:4%; padding-bottom: 4%; border-radius: 10px;">
+<div class="container"  align="center" style="width:800px; margin:0 auto; background: #d6d6d6; padding-top:2%; padding-bottom: 2%; border-radius: 10px; margin-top: 10%">
+    <div style="width: 80%; padding-top:4%; padding-bottom: 4%; border-radius: 10px; margin-bottom: 20px">
         <h2>Nomor surat Anda adalah : </h2>
-        <input type="text" name="nomor-surat" value="Hallo" disabled style="text-align: center;">
-        <button ><i class="fa fa-copy"></i></button>
+        <input type="text" name="nomor-surat" id="nomor-surat" value="III/FTIS/2021-06/0045-I" style="text-align: center; width: 60%;">
+        <button onclick="copyNoSurat()"><i class="fa fa-copy"></i></button> 
     </div>
 </div>
 
 </x-app-layout>
+
+<script>
+    function copyNoSurat() {
+        var copyText = document.getElementById("nomor-surat");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+    }
+</script>
