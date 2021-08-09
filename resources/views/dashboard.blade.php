df16c763643e06187ac16d47a624762fced50844
alwanrizky <alwanrizky7@gmail.com>
2021-08-09 13:44:41 +0700

    memindahkan view menu ke dalam dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,20 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{URL::asset('css/menu.css')}}"> 
 
-    <!-- content untuk dashboard -->
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+<x-app-layout>
+    <div class="container">
+        <div class="block" style="font-size=24px">
+            <a href="create-surat"><i class="fa fa-plus-circle fa-5x"></i></a><br>
+            <b>Create NoSurat</b>
+        </div>
+        <div class="block" style="font-size=24px">
+            <a href="#"><i class="fa fa-envelope fa-5x"></i></a><br>
+            <b>Template</b>
+        </div>
+        <div class="block" style="font-size=24px">
+            <a href="history"><i class="fa fa-history fa-5x"></i></a><br>
+            <b>History</b>
         </div>
     </div>
 </x-app-layout>
--- resources/views/generate-result.blade.php
+++ resources/views/generate-result.blade.php
@@ -4,7 +4,7 @@
 <div class="container"  align='center' style="width:800px; margin:0 auto; background: #d6d6d6; padding-top:2%; padding-bottom: 2%; border-radius: 10px; margin-top: 10%">
     <div style="width: 80%; padding-top:4%; padding-bottom: 4%; border-radius: 10px; margin-bottom: 20px">
         <h2>Nomor surat Anda adalah : </h2>
-        <input type="text" name="nomor-surat" id="nomor-surat" value="III/FTIS/2021-06/0045-I" style="text-align: center; width: 60%;">
+        <input type="text" name="nomor-surat" id="nomor-surat" value="III/FTIS/2021-06/0045-I" style="text-align: center; width: 60%;" readonly="readonly">
         <button onclick="copyNoSurat()"><i class="fa fa-copy"></i></button> 
     </div>
 </div>
