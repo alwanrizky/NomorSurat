272c8dd64dd02493e74a99f3df2a8dd4e8c50106
alwanrizky <alwanrizky7@gmail.com>
2021-08-13 11:11:47 +0700

    menginisialisasi tipe surat dan membuat fungsi untuk menampilkan create surat yang memanggil db

--- app/Http/Controllers/NomorSuratController.php
+++ app/Http/Controllers/NomorSuratController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\TipeSuratController;
+
 class NomorSuratController extends Controller
 {
-    //
+    private TipeSuratController $tipeSurat;
+
+    public function __construct(){
+        $this->tipeSurat = new TipeSuratController();
+    }
+
+    public function index(){
+        return view('create-surat', ['tipeSurat' => $this->tipeSurat->getTipeSurat()]);
+    }
 }
