4bc79cb820a69b6f78633a6f68a45def1433d500
alwanrizky <alwanrizky7@gmail.com>
2021-09-07 16:01:53 +0700

    Memperbaiki css untuk dashboard, agar menu bisa di tengah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,20 @@
 <link rel="stylesheet" href="{{URL::asset('css/menu.css')}}"> 
 
 <x-app-layout>
-    <div class="container">
-        <div class="menu">
-            <a href="create-surat"><i class="fa fa-plus-circle fa-5x"></i></a><br>
-            <b>Create NoSurat</b>
-        </div>
-        <div class="menu">
-            <a href="#"><i class="fa fa-envelope fa-5x"></i></a><br>
-            <b>Template</b>
-        </div>
-        <div class="menu">
-            <a href="history"><i class="fa fa-history fa-5x"></i></a><br>
-            <b>History</b>
-        </div>
+    <div class="container" style="width: 70%;">
+        <!-- <div class="vertical-center"> -->
+            <div class="menu">
+                <a href="create-surat"><i class="fa fa-plus-circle fa-5x"></i></a><br>
+                <b>Create NoSurat</b>
+            </div>
+            <div class="menu">
+                <a href="#"><i class="fa fa-envelope fa-5x"></i></a><br>
+                <b>Template</b>
+            </div>
+            <div class="menu">
+                <a href="history"><i class="fa fa-history fa-5x"></i></a><br>
+                <b>History</b>
+            </div>
+        <!-- </div> -->
     </div>
 </x-app-layout>
