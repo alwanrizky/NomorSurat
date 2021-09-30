d494992abd3471acf8b6ac7df809728e10acb818
alwanrizky <alwanrizky7@gmail.com>
2021-09-30 15:01:21 +0700

    membuat seeder untuk default admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'=>'Admin',
+            'email'=>'admin@admin.com',
+            'password'=>bcrypt('admin123'),
+            'is_admin'=>1,
+            'is_active'=>1
+
+        ]);
+    }
+}
