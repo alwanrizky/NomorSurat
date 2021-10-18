578a155c30c468dfc6ab7ff19cfae510164215f9
alwanrizky <alwanrizky7@gmail.com>
2021-10-18 18:00:52 +0700

    test

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,3 +1,4 @@
+trait
 <link rel="stylesheet" href="{{URL::asset('css/history.css')}}"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
