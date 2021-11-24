6d13dae4c308636f02af8dee071ad61b4e37c69c
alwanrizky <alwanrizky7@gmail.com>
2021-11-24 11:21:26 +0700

    menambahkan history controller untuk handle history nomor surat dan template surat

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HistoryController extends Controller
+{
+    private NomorSuratController $nomorSuratController;
+    private TemplateSuratController $templateSuratController;
+
+    public  function __construct()
+    {
+        $this->nomorSuratController = new NomorSuratController();
+        $this->templateSuratController =  new TemplateSuratController();
+    }
+
+    public function getHistory(){
+        $resNoSur = $this->nomorSuratController->getHistory();
+        $resTemp = $this->templateSuratController->getHistory();
+        return view('history', ['historyNomorSurat'=>$resNoSur[0], 
+        'tipeSurat' => $resNoSur[1],
+        'template'=>$this->templateSuratController->getTemplateSurat(),
+        'historyTemplateSurat'=>$resTemp]);
+    }
+
+    public function findHistoryNomorSurat(Request $request){
+        $resNoSur = $this->nomorSuratController->findHistory($request);
+        // $resTemp = $this->TemplateSuratController->findHistory($request);
+        return view('history', ['historyNomorSurat'=>$resNoSur[0],
+        'tipeSurat' => $resNoSur[1],
+        'template'=>$this->templateSuratController->getTemplateSurat()]);
+        // 'historyTemplateSurat'=>$resTemp]);
+    }
+}
