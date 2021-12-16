2663672fd3000e98b817778b275bf846a99da827
alwanrizky <alwanrizky7@gmail.com>
2021-12-16 16:41:11 +0700

    memperbaiki indentasi

--- resources/views/user-control.blade.php
+++ resources/views/user-control.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{URL::asset('css/history.css')}}"> 
+<link rel="stylesheet" href="{{URL::asset('css/history.css')}}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 
@@ -8,9 +8,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <x-app-layout>
     <!-- head -->
-    
 
-    <div class="container center" >
+
+    <div class="container center">
 
         <table class="center">
             <tr>
@@ -23,42 +23,38 @@
             </tr>
 
             <?php
-                $i = (($users->currentPage()-1)*20)+1
+            $i = (($users->currentPage() - 1) * 20) + 1
             ?>
             @foreach ($users as $u)
-                <tr>
-                    <td>{{$i}}</td>
-                    <td>{{$u['name']}}</td>
-                    <td>{{$u['email']}}</td>
-                    <td>
-                        <input name="admin-{{$u['id']}}" type="checkbox" disabled
-                            <?php
-                                if($u['is_admin']==1){
-                                    echo "checked";
-                                }
-                            ?>
-                        >
-                    </td>
+            <tr>
+                <td>{{$i}}</td>
+                <td>{{$u['name']}}</td>
+                <td>{{$u['email']}}</td>
+                <td>
+                    <input name="admin-{{$u['id']}}" type="checkbox" disabled <?php
+                                                                                if ($u['is_admin'] == 1) {
+                                                                                    echo "checked";
+                                                                                }
+                                                                                ?>>
+                </td>
 
-                    <td>
-                        <input name="active-{{$u['id']}}" type="checkbox" disabled
-                            <?php
-                                if($u['is_active']==1){
-                                    echo "checked";
-                                }
-                            ?>
-                            >
-                    </td>
-                    <td>
-                        <button class="btn btn-light" onclick="editUser({{$u}})" data-toggle="modal" data-target="#myModal"><i class="fa fa-edit fa-5">
-                    </td>
-                    
-                </tr>
-                <?php
-                    $i++;
-                ?>
+                <td>
+                    <input name="active-{{$u['id']}}" type="checkbox" disabled <?php
+                                                                                if ($u['is_active'] == 1) {
+                                                                                    echo "checked";
+                                                                                }
+                                                                                ?>>
+                </td>
+                <td>
+                    <button class="btn btn-light" onclick="editUser({{$u}})" data-toggle="modal" data-target="#myModal"><i class="fa fa-edit fa-5">
+                </td>
+
+            </tr>
+            <?php
+            $i++;
+            ?>
             @endforeach
-            
+
         </table>
         {{ $users->links() }}
         <br>
@@ -68,8 +64,8 @@
                     <input type="submit" id="change" name="change" value="CHANGE" class="btn btn-outline-dark" style="margin-right: 20px;">
                 </td>
                 <td style="border: none;"> -->
-                    <input type="submit" id="add-user" name="add-user" value="ADD USER" class="btn btn-outline-dark" data-toggle="modal" data-target="#myModal">
-                <!-- </td>
+        <input type="submit" id="add-user" name="add-user" value="ADD USER" class="btn btn-outline-dark" data-toggle="modal" data-target="#myModal">
+        <!-- </td>
             </tr>
         </table> -->
     </div>
@@ -86,13 +82,13 @@
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
                     <div class="modal-body">
-                
+
                         <form method="POST" id="formId" action="/user-control/add">
                             @csrf
 
                             <div class="mt-4">
                                 <x-jet-label for="name" value="{{ __('Name') }}" />
-                                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" required/>
+                                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" required />
                             </div>
 
                             <div class="mt-4">
@@ -102,7 +98,7 @@
 
                             <div class="mt-4">
                                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required/>
+                                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required />
                             </div>
 
                             <div class="mt-4">
@@ -122,20 +118,20 @@
                 </div>
             </div>
         </div>
-  
+
     </div>
 </x-app-layout>
 
 <script>
-    function editUser($u){
+    function editUser($u) {
         $('#header').text("Edit User");
-        $('#formId').attr('action', "/user-control/edit/"+$u['id']);
+        $('#formId').attr('action', "/user-control/edit/" + $u['id']);
         $('#name').attr('value', $u['name']);
         $('#email').attr('value', $u['email']);
-        $("#password").attr('disabled','disabled');
+        $("#password").attr('disabled', 'disabled');
         $("#password").hide();
-        $('#is_admin').prop('checked',$u['is_admin']);
-        $('#is_active').prop('checked',$u['is_active']);
+        $('#is_admin').prop('checked', $u['is_admin']);
+        $('#is_active').prop('checked', $u['is_active']);
         $('#btn').attr('value', "Save");
     }
 </script>
